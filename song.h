7d31fc500831a956b243549b850bf9d78f6d5106
Timothy-LiuXuefeng <65613511+Timothy-LiuXuefeng@users.noreply.github.com>
2020-05-21 00:04:51 +0800

    Update song.h

--- song.h
+++ song.h
@@ -45,29 +45,29 @@ struct song
 
 static song mainsong[]
 {
-    {mi1, 1, "©–"}, {so1, 2, "©–"}, {fa1, 1, "©–"}, {mi1, 1, "©–"}, {re1, 1.5, "©–"}, {mi1, 0.5, "©–"}, {fa1, 1, "©–"}, {zero, 0.5, "©–\n"},
-    {fa1, 0.5, "©–"}, {mi1, 1.5, "©–"}, {mi1, 0.5, "©–"}, {re1, 1.5, "©–"}, {re1, 0.5, "©–"}, {do1, 3, "©–\n"},
-    {so, 1, "Debout"}, {do1, 1.5, "! "}, {si, 0.5, "Les "}, {re1, 0.5, "damnes"}, {do1, 0.5, " "}, {so, 0.5, "de "}, {mi, 0.5, "la "}, {la, 2, "terre"}, {fa, 1, "! "}, {zero, 0.5, "\n"}, //Debout! Les damnes de la terre
-    {la, 0.5, "Debout"}, {re1, 1.5, "! "}, {do1, 0.5, "Les "}, {si, 0.5, "forcats"}, {la, 0.5, " "}, {so, 0.5, "de "}, {fa, 0.5, "la "}, {mi, 3, "faim! \n"}, //Debout! Les forcats de la faim
-    {so, 1, "La "}, {do1, 1.5, "raison"}, {si, 0.5, " "}, {re1, 0.5, "tonne "}, {do1, 0.5, "en "}, {so, 0.5, "son "}, {mi, 0.5, "cratere"}, {la, 2, ". "}, {fa, 0.5, "\n"},     //La raison tonne son cratere
-    {la, 0.5, ""}, {re1, 0.5, "C'est "}, {do1, 0.5, "l'eruption"}, {si, 1, " "}, {re1, 1, ""}, {fa1, 1, "de "}, {si, 1, "la "}, {do1, 2, "fin"}, {do1, 0.5, "! "}, {zero, 0.5, "\n"},  //C'est l'eruption de la fin
-    {mi1, 0.5, "Du "}, {re1, 0.5, "passe"}, {si, 2, " "}, {la, 0.5, "faisons"}, {si, 0.5, " "}, {do1, 0.5, "table"}, {la, 0.5, " "}, {si, 2, "rase. "}, {so, 0.5, "\n"},     //Du passe faisons table rase
-    {so, 0.5, ""}, {fa, 0.5, "Foule"}, {so, 0.5, " "}, {la, 1.5, "esclave! "}, {la, 0.5, "Debout"}, {re1, 1.5, "! "}, {do1, 0.5, "Debout"}, {si, 2, "!"}, {si, 0.5, " "}, {zero, 0.5, "\n"},     //Foule esclave, debout, debout
-    {re1, 1, "Le "}, {re1, 1.5, "monde"}, {si, 0.5, " "}, {so, 0.5, "va "}, {so, 0.5, "changer"}, {fa, 0.5, " "}, {so, 0.5, "de "}, {mi1, 2, "base. "}, {do1, 0.5, "\n"},  //Le monde va changer de base
-    {la, 0.5, "Nous "}, {si, 0.5, "ne "}, {do1, 0.5, "sommes"}, {si, 1, " "}, {re1, 1, "rien "}, {do1, 1, "soyons"}, {la, 1, " "}, {so, 2, "tout"}, {so, 0.5, "! "}, {zero, 0.5, "\n"}, //Nous ne sommes rien, soyons tout
+    {mi1, 1, "ã€‡"}, {so1, 2, "ã€‡"}, {fa1, 1, "ã€‡"}, {mi1, 1, "ã€‡"}, {re1, 1.5, "ã€‡"}, {mi1, 0.5, "ã€‡"}, {fa1, 1, "ã€‡"}, {zero, 0.5, "ã€‡\n"},
+    {fa1, 0.5, "ã€‡"}, {mi1, 1.5, "ã€‡"}, {mi1, 0.5, "ã€‡"}, {re1, 1.5, "ã€‡"}, {re1, 0.5, "ã€‡"}, {do1, 3, "ã€‡\n"},
+    {so, 1, "Debout"}, {do1, 1.5, "! "}, {xi, 0.5, "Les "}, {re1, 0.5, "damnes"}, {do1, 0.5, " "}, {so, 0.5, "de "}, {mi, 0.5, "la "}, {la, 2, "terre"}, {fa, 1, "! "}, {zero, 0.5, "\n"}, //Debout! Les damnes de la terre
+    {la, 0.5, "Debout"}, {re1, 1.5, "! "}, {do1, 0.5, "Les "}, {xi, 0.5, "forcats"}, {la, 0.5, " "}, {so, 0.5, "de "}, {fa, 0.5, "la "}, {mi, 3, "faim! \n"}, //Debout! Les forcats de la faim
+    {so, 1, "La "}, {do1, 1.5, "raison"}, {xi, 0.5, " "}, {re1, 0.5, "tonne "}, {do1, 0.5, "en "}, {so, 0.5, "son "}, {mi, 0.5, "cratere"}, {la, 2, ". "}, {fa, 0.5, "\n"},     //La raison tonne son cratere
+    {la, 0.5, ""}, {re1, 0.5, "C'est "}, {do1, 0.5, "l'eruption"}, {xi, 1, " "}, {re1, 1, ""}, {fa1, 1, "de "}, {xi, 1, "la "}, {do1, 2, "fin"}, {do1, 0.5, "! "}, {zero, 0.5, "\n"},  //C'est l'eruption de la fin
+    {mi1, 0.5, "Du "}, {re1, 0.5, "passe"}, {xi, 2, " "}, {la, 0.5, "faisons"}, {xi, 0.5, " "}, {do1, 0.5, "table"}, {la, 0.5, " "}, {xi, 2, "rase. "}, {so, 0.5, "\n"},     //Du passe faisons table rase
+    {so, 0.5, ""}, {fa, 0.5, "Foule"}, {so, 0.5, " "}, {la, 1.5, "esclave! "}, {la, 0.5, "Debout"}, {re1, 1.5, "! "}, {do1, 0.5, "Debout"}, {xi, 2, "!"}, {xi, 0.5, " "}, {zero, 0.5, "\n"},     //Foule esclave, debout, debout
+    {re1, 1, "Le "}, {re1, 1.5, "monde"}, {xi, 0.5, " "}, {so, 0.5, "va "}, {so, 0.5, "changer"}, {fa, 0.5, " "}, {so, 0.5, "de "}, {mi1, 2, "base. "}, {do1, 0.5, "\n"},  //Le monde va changer de base
+    {la, 0.5, "Nous "}, {xi, 0.5, "ne "}, {do1, 0.5, "sommes"}, {xi, 1, " "}, {re1, 1, "rien "}, {do1, 1, "soyons"}, {la, 1, " "}, {so, 2, "tout"}, {so, 0.5, "! "}, {zero, 0.5, "\n"}, //Nous ne sommes rien, soyons tout
     {mi1, 0.75, "C'est "}, {re1, 0.25, "la "}, {do1, 2, "lutte"}, {so, 1.5, " "}, {mi, 0.5, "finale"}, {la, 2, "."}, {fa, 0.5, " "}, {zero, 0.5, "\n"}, //C'est la lutte finale
-    {re1, 0.75, "Groupons"}, {do1, 0.25, "-"}, {si, 2, "nous "}, {la, 1, "et "}, {so, 1, "demain"}, {so, 2, "!"}, {so, 0.5, " "}, {zero, 0.5, "\n"}, //Groupons-nous et demain
-    {so, 1, "L'Internationale"}, {mi1, 2, ""}, {re1, 1, ""}, {so, 1, ""}, {do1, 2, ""}, {si, 1.5, " "}, //L'Internationale
-    {si, 0.5, "sera"}, {la, 1.5, " "}, {so, 0.5, "le "}, {la, 1, "genre "}, {re1, 1, "humain"}, {re1, 2, "!"}, {re1, 0.5, " "}, {zero, 0.5, "\n"},    //sera le genre humain
+    {re1, 0.75, "Groupons"}, {do1, 0.25, "-"}, {xi, 2, "nous "}, {la, 1, "et "}, {so, 1, "demain"}, {so, 2, "!"}, {so, 0.5, " "}, {zero, 0.5, "\n"}, //Groupons-nous et demain
+    {so, 1, "L'Internationale"}, {mi1, 2, ""}, {re1, 1, ""}, {so, 1, ""}, {do1, 2, ""}, {xi, 1.5, " "}, //L'Internationale
+    {xi, 0.5, "sera"}, {la, 1.5, " "}, {so, 0.5, "le "}, {la, 1, "genre "}, {re1, 1, "humain"}, {re1, 2, "!"}, {re1, 0.5, " "}, {zero, 0.5, "\n"},    //sera le genre humain
     {mi1, 0.75, "C'est "}, {re1, 0.25, "la "}, {do1, 2, "lutte"}, {so, 1.5, " "}, {mi, 0.5, "finale"}, {la, 2, "."}, {fa, 0.5, " "}, {zero, 0.5, "\n"}, //C'est la lutte finale
-    {re1, 0.75, "Groupons"}, {do1, 0.25, "-"}, {si, 2, "nous "}, {la, 1, "et "}, {so, 1, "demain"}, {mi1, 3,"! \n"}, //Groupons-nous et demain
+    {re1, 0.75, "Groupons"}, {do1, 0.25, "-"}, {xi, 2, "nous "}, {la, 1, "et "}, {so, 1, "demain"}, {mi1, 3,"! \n"}, //Groupons-nous et demain
     {mi1, 1, "L'Internationale"}, {so1, 2, ""}, {fa1, 1, ""}, {mi1, 1, ""}, {re1, 1.5, ""}, {mi1, 0.5, ""}, {fa1, 1, ""}, {zero, 0.5, " "}, //L'Internationale
     {fa1, 0.5, "sera"}, {mi1, 1.5, " "}, {mi1, 0.5, "le "}, {re1, 1.5, "genre "}, {re1, 0.5, "humain! "}, {do1, 4, "\n"}, //Sera le genre humain
 }; 
 
-extern std::vector<song> inputSong; //¶ÁÈë¸èÇú
+extern std::vector<song> inputSong; //è¯»å…¥æ­Œæ›²
 
-int readSong(const char* src);    //¶ÁÈë¸èÇú
+int readSong(const char* src);    //è¯»å…¥æ­Œæ›²
 
 
 #endif
