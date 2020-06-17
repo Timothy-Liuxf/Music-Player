b2c8299baf972b82850d73edb80ed64d2cbd2a2e
Timothy-LiuXuefeng <65613511+Timothy-LiuXuefeng@users.noreply.github.com>
2020-06-18 03:51:55 +0800

    Update str.h

--- str.h
+++ str.h
@@ -4,10 +4,12 @@
 #define STR_H
 
 static char start[] = "What do you want to do? \nA.Start playing the music  B.About  C.Exit";
-static char about[] = "Name: Music Player\nAuthor: Timothy Liu\nVersion: v2.0";
-static char illegal[] = "Input illegal!";
-static char file_not_exist[] = "Cannot find the file: ";
+static char about[] = "Name: Music Player\nAuthor: Timothy Liu\nVersion: v3.0";
+static char illegal[] = "Input illegal!"; 
+static char file_not_exist[] = "Cannot find the file: "; 
 static char parse_failed[] = "Syntax parsing error! In: "; 
 static char please_input[] = "Please input the source file name: "; 
+static char parsing[] = "Parsing the file "; 
+static char parsed[] = "Successfully parsed the file "; 
 
 #endif
